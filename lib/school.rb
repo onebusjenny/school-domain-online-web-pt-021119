3aeb8f48bb13352822ef11f6195b5c65b92f9d42
Jenny Chu <jchudesign0930@gmail.com>
2019-03-16 02:33:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,5 +3,10 @@ class School
  school = School.new( "Bayside High School")
  school.add_student("Zach Morris", 9)
  school.roster
+ hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+ 
+hash
+ => {"new_key"=>["new_value_for_value_array"]} 
  
 end
