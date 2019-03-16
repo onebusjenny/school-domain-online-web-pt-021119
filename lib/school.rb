278fb5627a433c7beb56aa51c88923fd426a6456
Jenny Chu <jchudesign0930@gmail.com>
2019-03-16 01:38:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,15 @@
 class School
   
+  
+  def initialize(model)
+    @model = model
+    end
+  
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
 
   
   def add_student (name,year)
